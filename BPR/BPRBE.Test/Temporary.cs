e06ae13c58d65b1de23c54d036d26a92441fc389
RealFoXsTeRDOTA <foxsterdota@gmail.com>
2023-09-13 10:39:33 +0200

    Add NUnit test project

---
+++ BPR/BPRBE.Test/Temporary.cs
@@ -0,0 +1,37 @@
+﻿namespace BPRBE.Tests;
+
+[TestFixture]
+internal class Temporary
+{
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+    }
+
+    [Test]
+    public void MethodName_WhenSomething_ThenSomething()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void MethodName2_WhenSomething_ThenSomething()
+    {
+        Assert.Fail();
+    }
+}
