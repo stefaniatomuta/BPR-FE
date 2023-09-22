102d8aa557f37be2f6a25e14756cb2f75d0e1f70
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-09-22 11:21:41 +0200

    Autoincrement id

--- BPRBE/Persistence/MongoDependencyRepository.cs
+++ BPRBE/Persistence/MongoDependencyRepository.cs
@@ -1,6 +1,8 @@
 ﻿using BPRBE.Config;
+using BPRBE.Constants;
 using BPRBE.Models.Persistence;
 using Microsoft.Extensions.Options;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace BPRBE.Persistence;
@@ -8,6 +10,7 @@ namespace BPRBE.Persistence;
 public class MongoDependencyRepository : IMongoDependencyRepository
 {
     private readonly IMongoCollection<ArchitecturalModel> _dependenciesRuleCollection;
+    private readonly IMongoCollection<BsonDocument> _sequenceCollection;
 
     public MongoDependencyRepository(IOptions<DatabaseConfig> databaseSettings)
     {
@@ -16,6 +19,7 @@ public class MongoDependencyRepository : IMongoDependencyRepository
 
         _dependenciesRuleCollection =
             mongoDatabase.GetCollection<ArchitecturalModel>(databaseSettings.Value.DependenciesRuleCollectionName);
+        _sequenceCollection = mongoDatabase.GetCollection<BsonDocument>(databaseSettings.Value.DependenciesRuleCollectionName);
     }
 
     public async Task<IList<ArchitecturalModel>> GetArchitecturalModelsAsync()
@@ -31,7 +35,17 @@ public class MongoDependencyRepository : IMongoDependencyRepository
         var result = await GetArchitecturalModelByName(model);
         if (result == null)
         {
+            model.Id =  await GetNextSequenceValueAsync();
             await _dependenciesRuleCollection.InsertOneAsync(model);
         }
+    }
+    private async Task<int> GetNextSequenceValueAsync()
+    {
+        var filter = Builders<BsonDocument>.Filter.Eq(Values.MongoDbId, Values.MongoDbId);
+        var update = Builders<BsonDocument>.Update.Inc(Values.MongoDbValue, 1);
+        var options = new FindOneAndUpdateOptions<BsonDocument, BsonDocument> { IsUpsert = true, ReturnDocument = ReturnDocument.After };
+        var result = await _sequenceCollection.FindOneAndUpdateAsync(filter, update, options);
+
+        return result[Values.MongoDbValue].AsInt32;
     }
 }
