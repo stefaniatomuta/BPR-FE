b8560c60f6ac33b27b97e095334337d48414e8ff
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-09-27 21:32:00 +0200

    Added SelectArchitectureComponent.razor

---
+++ BPRBlazor/Components/AnalysisSetup/SelectArchitectureComponent.razor.cs
@@ -0,0 +1,29 @@
+﻿using BPRBE.Models.Persistence;
+using Microsoft.AspNetCore.Components;
+
+namespace BPRBlazor.Components.AnalysisSetup;
+
+public partial class SelectArchitectureComponent : ComponentBase
+{
+    [Parameter]
+    public string SelectedArchitecturalModel { get; set; } = default!;
+
+    public IList<ArchitecturalModel> architecturalOption = new List<ArchitecturalModel>();
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        architecturalOption = await GetArchitecturalModels();
+    }
+
+    private async Task<IList<ArchitecturalModel>> GetArchitecturalModels()
+    {
+        return await DependencyRepository.GetArchitecturalModelsAsync();
+    }
+    public async Task OnSelectedValueChangedAsync(ChangeEventArgs e)
+    {
+        SelectedArchitecturalModel = e.Value.ToString();
+        Console.WriteLine("It is definitely: " + SelectedArchitecturalModel);
+        StateHasChanged();
+    }
+}
