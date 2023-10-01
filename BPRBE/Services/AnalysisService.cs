db4825e37b0b730c5b621d80fe03dac2ceeb8c13
Sombrero <adriana@grecea.net>
2023-10-01 21:21:13 +0200

    BPR-68: add analysis services

---
+++ BPRBE/Services/AnalysisService.cs
@@ -0,0 +1,40 @@
+﻿using BPR.Models.Analysis;
+using BPR.Models.Blazor;
+using BPR.Models.Enums;
+
+namespace BPRBE.Services;
+
+public class AnalysisService : IAnalysisService {
+
+    private ICodeExtractionService _codeExtractionService;
+
+    public AnalysisService(ICodeExtractionService codeExtractionService) {
+        _codeExtractionService = codeExtractionService;
+    }
+
+
+    public List<ViolationModel> GetDependencyAnalysis(string folderPath, ArchitecturalModel model) {
+        List<ViolationModel> violations = new();
+        foreach (var component in model.Components) {
+            violations.AddRange(GetViolationsForComponentDependency(component.Name, component.NamespaceComponents));
+        }
+        return violations;
+    }
+
+    private List<ViolationModel> GetViolationsForComponentDependency(string componentPath, List<Namespace> namespaces) {
+        List<ViolationModel> notMatched = new ();
+        var usings = _codeExtractionService.GetUsingDirectives(componentPath);
+        foreach (var directive in usings) {
+            if (!namespaces.Select(n => n.Name).Any(n => n.Contains(directive))) {
+                var violation = new ViolationModel{
+                    Type = ViolationType.ForbiddenDependencyDirection,
+                    Name = componentPath,
+                    Severity = ViolationSeverity.Major,
+                    Code = directive
+                };
+                notMatched.Add(violation);
+            }
+        }
+        return notMatched;
+    }
+}
---
+++ BPRBE/Services/IAnalysisService.cs
@@ -0,0 +1,16 @@
+﻿using BPR.Models.Analysis;
+using BPR.Models.Blazor;
+using BPRBE.Models;
+
+namespace BPRBE.Services; 
+
+public interface IAnalysisService {
+    /// <summary>
+    /// Takes an architecture model and compares the list of dependencies for each component with the list of using directives from each cs file in the directive
+    /// A violation of dependency is a using directive that does match not any of the dependencies related to the component
+    /// </summary>
+    /// <param name="folderPath">The path to the root temp directory</param>
+    /// <param name="model">Architectural model </param>
+    /// <returns>A list of all violations and their severity</returns>
+    List<ViolationModel> GetDependencyAnalysis(string folderPath, ArchitecturalModel model);
+}
