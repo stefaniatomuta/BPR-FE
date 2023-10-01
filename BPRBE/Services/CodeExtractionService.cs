35ffb7b1151f8f0b5b146147f2c1f9a232c8a758
Sombrero <adriana@grecea.net>
2023-10-01 15:30:36 +0200

    BPR-68: add code extraction service

---
+++ BPRBE/Services/CodeExtractionService.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BPRBE.Services; 
+
+public class CodeExtractionService : ICodeExtractionService {
+    private string usingRegex = @"^using\s+[A-Za-z_][A-Za-z0-9_]*(?:\.[A-Za-z_][A-Za-z0-9_]*)*;$";
+    private string namespaceRegex = @"^\s*namespace\s+[A-Za-z_][A-Za-z0-9_.]*\s*";
+    
+    public List<string> GetUsingDirectives(string filepath) {
+        List<string> matches = new ();
+        var files  = Directory.EnumerateFiles(filepath,SearchOption.AllDirectories.ToString());
+        foreach (var file in files) {
+            if (file.EndsWith(".cs")) {
+                var content = File.ReadAllText(file);
+                var result = Regex.Matches(content, usingRegex);
+                matches.AddRange(result.Select(r =>r.Value));
+            }
+        }
+        return matches;
+    }
+}
---
+++ BPRBE/Services/ICodeExtractionService.cs
@@ -0,0 +1,10 @@
+﻿namespace BPRBE.Services; 
+
+public interface ICodeExtractionService {
+    /// <summary>
+    /// Returns all using directives in all the children files of a given path
+    /// </summary>
+    /// <param name="filepath">path to the parent folder  </param>
+    /// <returns>list of using directives</returns>
+    List<string> GetUsingDirectives(string filepath);
+}
