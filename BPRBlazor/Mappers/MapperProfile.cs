4112fc89d7334efd06bc786e3175d6931bd27621
Sombrero <adriana@grecea.net>
2023-10-08 22:04:45 +0200

    BPR-68: added mapper

---
+++ BPRBlazor/Mappers/MapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BPR.Analysis.Models;
+using BPR.Models.Blazor;
+using BPRBlazor.Models;
+
+
+namespace BPRBlazor.Mappers; 
+
+public class MapperProfile : Profile {
+    public MapperProfile() {
+        CreateMap<AnalysisArchitecturalComponent, ArchitecturalComponentViewModel>().ReverseMap();
+        CreateMap<AnalysisArchitecturalModel, ArchitecturalModelViewModel>().ReverseMap();
+        CreateMap<AnalysisNamespace, NamespaceViewModel>().ReverseMap();
+    }
+}
