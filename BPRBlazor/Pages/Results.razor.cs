abcafe04b48b5a2eb93010de6fffaae52b406a47
Sombrero <adriana@grecea.net>
2023-10-10 22:28:08 +0200

    BPR-68: add unsubscribe for state

--- BPRBlazor/Pages/Results.razor.cs
+++ BPRBlazor/Pages/Results.razor.cs
@@ -1,5 +1,4 @@
-﻿using BPRBE.Enums;
-using BPRBE.Models;
+﻿using BPRBE.Models;
 using Microsoft.AspNetCore.Components;
 
 namespace BPRBlazor.Pages;
@@ -12,28 +11,11 @@ public partial class Results : ComponentBase
     {
         base.OnInitialized();
         StateContainer.OnChange += StateHasChanged;
-        // _resultModel = GenerateResultModel();
         _resultModel = new ResultModel();
         _resultModel.Violations = StateContainer.Property;
     }
 
-    // private static ResultModel GenerateResultModel()
-    // {
-    //     var score = new Random()
-    //         .NextDouble() * 100;
-    //
-    //     // var violations = GenerateViolations();
-    //
-    //     return new(score, );
-    // }
-
-    // private static List<ViolationModel> GenerateViolations()
-    // {
-    //     return new List<ViolationModel>
-    //     {
-    //         new(1, "Violation name 1", ViolationType.ForbiddenDependencyDirection, "A class from the 'Models' namespace must not depend on a class in the 'Controllers' namespace", ViolationSeverity.Major, "using Controllers;\n\nnamespace Models;\n\npublic class One {\n    private IDependency _dependency;\n    \n    public One(IDependency dependency) {\n        _dependency = dependency;\n    }"),
-    //         new(2, "Violation name 2", ViolationType.Unknown, "Some other thing I have no idea...", ViolationSeverity.Minor, "public class Two {\n    private IDependency _dependency;\n    \n    public One(IDependency dependency) {\n        _dependency = dependency;\n    }"),
-    //         new(3, "Violation name 3", ViolationType.Unknown, "Some other thing I have no idea...", ViolationSeverity.Critical, "public class Two {\n    private IDependency _dependency;\n    \n    public One(IDependency dependency) {\n        _dependency = dependency;\n    }"),
-    //     };
-    // }
+    public void Dispose() {
+        StateContainer.OnChange -= StateHasChanged;
+    }
 }
