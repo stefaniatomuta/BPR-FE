93b5b1a0ce918e00f2ff7802e2f1644fc43fa56c
Sombrero <adriana@grecea.net>
2023-10-11 13:56:35 +0200

    BPR-68: refactorings

--- BPR.Analysis/Models/AnalysisRegex.cs
+++ BPR.Analysis/Models/AnalysisRegex.cs
@@ -1,6 +1,6 @@
 ﻿namespace BPR.Analysis.Models; 
 
-public class AnalysisRegex {
+public static class AnalysisRegex {
     public static string usingRegex { get; } = @"^using\s+[A-Za-z_][A-Za-z0-9_]*(?:\.[A-Za-z_][A-Za-z0-9_]*)*;$";
     public static string namespaceRegex { get;  }= @"^\s*namespace\s+[A-Za-z_][A-Za-z0-9_.]*\s*";
 }
