ae72e42c3c9a7862559db9759ccb0e327073f76f
vladdirlea2002 <dirlea.ioan.vlad@gmail.com>
2023-12-02 14:21:02 +0100

    BPR-134: fix enum get description

---
+++ BPR.Mediator/Utils/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace BPR.Mediator.Utils;
+
+public class EnumExtensions
+{
+    public static string GetDescription(Enum enumValue)
+    {
+        var field = enumValue.GetType().GetField(enumValue.ToString());
+        if (field == null)
+            return enumValue.ToString();
+
+        if (Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attribute)
+        {
+            return attribute.Description;
+        }
+
+        return enumValue.ToString();
+    }
+}
